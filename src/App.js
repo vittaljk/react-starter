a8afb104b80acfeb952dd6f76bb3c65bc7805eb1
Vijay Vittal J Kashinath <vkashinath@celegence.com>
2019-06-13 17:17:58 +0530

    clean up for sri frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import TodoList from './Todo/containers/TodoList';
-import PlayGround from './playground/playground';
 import 'antd/dist/antd.css';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <Switch>
       <Route path="/todo-list" component={TodoList} />
       <Route path="/" exact component={TodoList} />
-      <Route path="/playground" component={PlayGround} />
       <Redirect to="/" />
     </Switch>
   );
